9d19b8b91e04ee97aca7b92c8ce3b73414d9c9d8
Andrew Stride <andrewjonathanstride@gmail.com>
2025-04-14 12:25:29 +0100

    add parse args func and tests

--- src/local_invoke.py
+++ src/local_invoke.py
@@ -1,25 +1,43 @@
 import argparse
 
-def parse_args(arg_list: list[str] | None = None):
+
+def parse_args(arg_list: list[str] | None = None) -> dict | None:
+    """parse given args or sys.args for Search Query, optional Date - YYYY-MM-DD, and Reference
+
+    Args:
+        arg_list (list[str] | None, optional): List of strings using flags to declare args: -q query, -d date, -ref reference
+
+    Returns:
+        _type_: args Dict | None
+    """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('query', type=str, nargs='+', help='search query')
-    parser.add_argument('-d', type=str, help='enter date from (YYYY-MM-DD)?')
-    parser.add_argument('reference', type=str, help='reference')
+    parser.add_argument("-q", type=str, nargs="+", help="search query")
+    parser.add_argument("-d", type=str, help="enter date from (YYYY-MM-DD)?")
+    parser.add_argument("-ref", type=str, help="one word reference")
+    try:
+        args = vars(parser.parse_args(arg_list))
+        if not args["q"] or not args["ref"]:
+            raise argparse.ArgumentError("-q Query and -ref Reference required")
+        for k, v in args.items():
+            if isinstance(v, list):
+                args[k] = "%20".join([s for s in v])
+        if args["d"] == None:
+            args.pop("d")
+        return args
+    except:
+        return None
 
-    args = parser.parse_args(arg_list)
-    print(f"{args=}")
-    return vars(args)
 
 def main():
     # parse given args / handle malformed request/arg
     # Invoke Lambda with JSON event
     # Handle response
-        # print details of results found & added to queue
-        # OR 
-        # details of error?
-    print("oy")
+    # print details of results found & added to queue
+    # OR
+    # details of error?
     parse_args()
+
 
 if __name__ == "__main__":
     main()
--- test/test_local_invoke.py
+++ test/test_local_invoke.py
@@ -1,6 +1,78 @@
 from src.local_invoke import main, parse_args
 import shlex
+import pytest
+
 
 class TestParseArgs:
     def test_returns_dict(self):
-        assert isinstance(parse_args(['q','r']), dict)
+        assert isinstance(parse_args(shlex.split("-q q -ref ref")), dict)
+
+    def test_returns_none_if_no_args(self):
+        assert parse_args() == None
+
+    def test_returns_dict_with_query_and_reference_keys(self):
+        test_input = shlex.split("-q test -r ref")
+        output = parse_args(test_input)
+        assert list(output.keys()) == ["q", "ref"]
+
+    def test_returns_dict_with_date_key_if_provided(self):
+        test_input = shlex.split("-q test -r ref -d 1997-01-01")
+        output = parse_args(test_input)
+        assert "d" in list(output.keys())
+
+    def test_no_date_given_returns_dict_without_date_key(self):
+        test_input = shlex.split("-q test -ref ref")
+        output = parse_args(test_input)
+        assert "d" not in list(output.keys())
+
+    def test_output_dict_vals_are_strings(self):
+        test_input = shlex.split("-q test -ref ref")
+        output = parse_args(test_input)
+        for v in output.values():
+            assert isinstance(v, str)
+
+    @pytest.mark.parametrize(
+        "test_input,expected",
+        [
+            ("-q two words -ref ref", "two%20words"),
+            ("-q three word query -ref ref", "three%20word%20query"),
+        ],
+    )
+    def test_joins_multiple_word_query_with_percent_20(self, test_input, expected):
+        output = parse_args(shlex.split(test_input))
+        assert output["q"] == expected
+
+    expected_dict = {"q": "query", "d": "1997-01-01", "ref": "ref"}
+
+    @pytest.mark.parametrize(
+        "test_input,expected",
+        [
+            ("-q query -ref ref -d 1997-01-01", expected_dict),
+            ("-ref ref -q query -d 1997-01-01", expected_dict),
+            ("-d 1997-01-01 -ref ref -q query", expected_dict),
+        ],
+    )
+    def test_handles_arg_orders(self, test_input, expected):
+        output = parse_args(shlex.split(test_input))
+        assert output == expected
+
+    @pytest.mark.parametrize(
+        "test_input,expected",
+        [
+            (
+                "-q two words -d 1997-01-01 -ref ref",
+                {"q": "two%20words", "d": "1997-01-01", "ref": "ref"},
+            ),
+            (
+                "-q three word query -d 1997-01-01 -ref ref",
+                {"q": "three%20word%20query", "d": "1997-01-01", "ref": "ref"},
+            ),
+        ],
+    )
+    def test_handles_arg_order_and_multiple_word_query(self, test_input, expected):
+        output = parse_args(shlex.split(test_input))
+        assert output == expected
+
+    def test_handles_multiple_word_reference(self):
+        test_input = shlex.split("-q query -ref three word ref")
+        assert parse_args(test_input) == None
