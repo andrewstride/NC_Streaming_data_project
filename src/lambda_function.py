378234dea6a511bfc7b3abb41b5b4e1aff140930
Andrew Stride <andrewjonathanstride@gmail.com>
2025-04-24 12:34:40 +0100

    add error handling to utils and tests

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,22 +1,86 @@
 import os
+import logging
+import requests
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+BASE_URL = "https://content.guardianapis.com/search?"
 
 def lambda_handler(event, context):
-    pass
-    # Get ENV variables
+    # Handle event
+    try:
+        query = event['q']
+        date = event.get('d', None)
+        reference = event['ref']
+    except KeyError as e:
+        missing_key = e.args[0]
+        logger.error("Missing required event key: %s", missing_key)
+        return {
+            "statusCode": 400,
+            "error": "Bad request",
+            "message": f"Missing required event key: {missing_key} - 'q' and 'ref' required"
+        }
+    # Get ENV vars
+    try:
+        api_key, sqs_queue_url = _env_variables()
+    except KeyError as e:
+        missing_key = e.args[0]
+        return {
+            "statusCode": 500,
+            "error": "Internal server error",
+            "message": f"Missing required environment variable: {missing_key}"
+        }
+
     # Build URL
+    url = _build_url(query, api_key, date)
+
     # Collect response from Guardian API
+    # response = requests.get(url)
+    # print(response)
+
     # For each result:
-        # Process into dict
-        # Send to SQS queue
+        # Process into dict:
+
+            # webPublicationDate
+            # webTitle
+            # webUrl
+            # reference
+            # Convert to JSON and Insert up to 10 messages into SQS
+            # (automatically ordered by recent first and 10 per page)
+
+            
+        # Send to SQS queue - POST request? Boto3?
+    
+    # return {
+    #     'numberOfMessages': x,
+    #     'error': "error details if error"
+    #     'messages': [
+    #         {
+    #             "contents of each message": "foo"
+    #         },
+    #         {
+    #             "contents of each message": "bar"
+    #         }
+    #     ]
+    # }
+    
 
 
 def _env_variables():
-    api_key = os.environ.get("api_key")
-    sqs_queue_url = os.environ.get("sqs_queue_url")
-    return {
-        "api_key": api_key,
-        "sqs_queue_url": sqs_queue_url
-    }
+    required_vars = ['api_key', 'sqs_queue_url']
+    env_vars = {}
 
-def _build_url(query, reference, api_key, date=None):
-    return query + reference + api_key + date
+    for var in required_vars:
+        val = os.environ.get(var)
+        if not val:
+            raise KeyError(f"Missing environment variable: {var}")
+        env_vars[var] = val
+        
+    return env_vars['api_key'], env_vars['sqs_queue_url']
+
+def _build_url(query, api_key, date=None):
+    url = f"{BASE_URL}q={query}"
+    if date:
+        url += f"&from-date={date}"
+    return url + f"&api-key={api_key}"
--- test/test_lambda_handler.py
+++ test/test_lambda_handler.py
@@ -1,6 +1,8 @@
-from src.lambda_function import lambda_handler, _env_variables, _build_url
+from src.lambda_function import lambda_handler, _env_variables, _build_url, BASE_URL
 from unittest.mock import patch
 import pytest
+import re
+import requests
 
 
 class TestEnvVariablesUtil:
@@ -24,21 +26,55 @@ class TestEnvVariablesUtil:
     def test_collects_values(self, monkeypatch):
         monkeypatch.setenv("api_key", "test_key")
         monkeypatch.setenv("sqs_queue_url", "test_url")
-        output = _env_variables()
-        assert output.get("api_key") == "test_key"
-        assert output.get("sqs_queue_url") == "test_url"
+        api_key, sqs_queue_url = _env_variables()
+        assert api_key == "test_key"
+        assert sqs_queue_url == "test_url"
+
+    def test_raises_exception_if_sqs_var_not_found(self, monkeypatch):
+        monkeypatch.setenv("api_key", "test_key")
+        with pytest.raises(KeyError) as e:
+            _env_variables()
+        assert "Missing environment variable: sqs_queue_url" in str(e.value)
+
+    def test_raises_exception_if_api_key_var_not_found(self, monkeypatch):
+        monkeypatch.setenv("sqs_queue_url", "test_url")
+        with pytest.raises(KeyError) as e:
+            _env_variables()
+        assert "Missing environment variable: api_key" in str(e.value)
+
+
+class TestBASEURL:
+    def test_returns_200(self):
+        response = requests.get(BASE_URL + "&api-key=test")
+        assert response.status_code == 200
 
 class TestBuildUrl:
     def test_returns_string(self):
-        args = ['test_query', "test_ref", "test_api_key"]
+        args = ['test_query', "test_api_key"]
         kwargs= {"date": '1997-01-01'}
         assert isinstance(_build_url(*args, **kwargs), str)
 
+    def test_url_format_correct_with_date(self):
+        url = _build_url("test", "test", '1997-01-01')
+        pattern = r"https:\/\/content.guardianapis.com\/search\?q=[a-z]+&from-date=[\d]{4}-[\d]{2}-[\d]{2}&api-key=test"
+        assert re.match(pattern, url)
+
+    def test_url_format_correct_without_date(self):
+        url = _build_url("test", "test")
+        pattern = r"https:\/\/content.guardianapis.com\/search\?q=[a-z]+&api-key=test"
+        assert re.match(pattern, url)
+
+
 class TestHandler:
-    def test_accesses_env_variables(self):
-        # set env variables for Guardian API key & SQS Queue name
-        # mock os.environ.get.called_with
-        lambda_handler({}, {})
+    @patch('src.lambda_function.requests')
+    def test_returns_400_for_malformed_event(self, mock_requests):
+        output = lambda_handler({}, {})
+        expected = {
+            "statusCode": 400,
+            "error": "Bad request",
+            "message": "Missing required event key: q - 'q' and 'ref' required"
+        }
+        assert output == expected
 
 @pytest.fixture(scope="function")
 def event_no_date():
