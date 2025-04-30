07048a42f9cd144f1e4dc7be4381a44da763ab71
Andrew Stride <andrewjonathanstride@gmail.com>
2025-04-30 22:03:32 +0100

    debug cicd aws region issue

--- test/test_lambda_handler.py
+++ test/test_lambda_handler.py
@@ -447,7 +447,7 @@ def message():
 @pytest.fixture(scope="function")
 def mock_sqs_moto_and_url_in_env(monkeypatch):
     with mock_aws():
-        monkeypatch.setenv("AWS_REGION", "eu-west-2")
+        print(os.environ.get("AWS_REGION"))
         conn = boto3.client("sqs")
         response = conn.create_queue(
             QueueName="test_queue.fifo",
