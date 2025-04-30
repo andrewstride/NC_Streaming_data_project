685bcda62e6093df4ffcc4f2747510fa4d8b3520
Andrew Stride <andrewjonathanstride@gmail.com>
2025-04-30 20:34:50 +0100

    add lambda layer

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -53,7 +53,7 @@ resource "aws_lambda_function" "guardian_api_lambda" {
   function_name = var.lambda_name
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
-
+  layers = [aws_lambda_layer_version.lambda_layer.arn]
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.12"
@@ -71,4 +71,12 @@ output "lambda_function_arn" {
   description = "ARN of Guardian API Lambda"
   value = try(aws_lambda_function.guardian_api_lambda.arn)
   sensitive = true
+}
+
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename   = "layer.zip"
+  layer_name = "lambda_layer_name"
+
+  compatible_runtimes = ["python3.12"]
+  source_code_hash = filebase64sha256("layer.zip")
 }
