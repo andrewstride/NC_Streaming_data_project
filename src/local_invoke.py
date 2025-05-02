4556b45239e93ed574d24812c6431d5c76965396
Andrew Stride <andrewjonathanstride@gmail.com>
2025-05-02 15:48:55 +0100

    add handle response util

--- src/local_invoke.py
+++ src/local_invoke.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+import botocore.client
+from dotenv import load_dotenv
+import botocore
 import argparse
 import sys
 import json
 import os
 import boto3
-from datetime import datetime
-from dotenv import load_dotenv
 
 
 def parse_args(arg_list: list[str] | None = None) -> dict | None:
@@ -34,15 +36,14 @@ def parse_args(arg_list: list[str] | None = None) -> dict | None:
         for k, v in args.items():
             if isinstance(v, list):
                 args[k] = "%20".join([s for s in v])
-        if args["d"] is None or not _is_valid_date(args["d"]):
+        if args["d"] is None or not is_valid_date(args["d"]):
             args.pop("d")
         return args
     except Exception as e:
         print(e)
-        return None
 
 
-def _is_valid_date(date: str) -> bool:
+def is_valid_date(date: str) -> bool:
     try:
         return bool(datetime.strptime(date, "%Y-%m-%d"))
     except (ValueError, TypeError):
@@ -61,10 +62,10 @@ def request_args() -> dict:
     date = None
     if input("Enter date from? (y/n): ") == "y":
         date = input("Enter date (YYYY-MM-DD): ")
-        if not _is_valid_date(date):
+        if not is_valid_date(date):
             date = input("Please try again (YYYY-MM-DD): ")
 
-    if _is_valid_date(date):
+    if is_valid_date(date):
         print(f"Date: {date}")
     else:
         print("Invalid date. Will not be included")
@@ -72,7 +73,7 @@ def request_args() -> dict:
 
     ref = input("Enter one word reference: ")
     if " " in ref:
-        ref = _spaces_replaced(ref)
+        ref = spaces_replaced(ref)
     print(f"Reference: {ref}")
 
     args = {"q": query, "ref": ref}
@@ -83,7 +84,7 @@ def request_args() -> dict:
     return args
 
 
-def _spaces_replaced(string):
+def spaces_replaced(string):
     output = ""
     for i in range(len(string)):
         if string[i] == " ":
@@ -93,27 +94,34 @@ def _spaces_replaced(string):
     return output
 
 
-def invoke_lambda(lambda_client: boto3.client, lambda_id: str, args: dict) -> dict:
+def invoke_lambda(
+    lambda_client: botocore.client.BaseClient, lambda_id: str, args: dict
+) -> dict:
     """Invoke lambda function and return response
 
     Args:
+        lambda_client (BaseClient): A Boto3 Lambda client
         lambda_id (str): Lambda name or ARN
-        args (dict): {'q': query, ('d': date,) 'ref': ref}
+        args (dict): Invocation payload e.g. {'q': query, ('d': date) 'ref': ref}
 
     Returns:
         dict: response
     """
 
-    response = lambda_client.invoke(
-        FunctionName=lambda_id,
-        InvocationType="RequestResponse",
-        LogType="Tail",
-        Payload=json.dumps(args),
-    )
-    return response
+    try:
+        response = lambda_client.invoke(
+            FunctionName=lambda_id,
+            InvocationType="RequestResponse",
+            LogType="Tail",
+            Payload=json.dumps(args),
+        )
+        return response
+    except Exception as e:
+        print("Error invoking Lambda")
+        raise RuntimeError(f"Failed to invoke Lambda: {e}")
 
 
-def _lambda_name() -> str:
+def lambda_name() -> str:
     load_dotenv()
     name = os.environ.get("LAMBDA_NAME")
     if not name:
@@ -121,17 +129,40 @@ def _lambda_name() -> str:
     return name
 
 
-def main():
-    # print details of results found & added to queue
-    # OR
-    # details of error?
+def get_args():
     args = parse_args()
     if not args:
         args = request_args()
+    return args
+
+
+def handle_lambda_response(response: dict) -> None:
+    status_code = response.get("StatusCode")
+    if status_code == 200:
+        try:
+            payload = json.loads(response["Payload"].read())
+            if payload["statusCode"] == 200:
+                print("Successful response")
+                print(f"{payload['messagesSent']} message(s) sent")
+                print(f"{payload['messagesFailed']} message(s) failed")
+                print("Messages sent:")
+                for m in payload["messages"]:
+                    print(m)
+        except Exception as e:
+            print(f"Error handling payload: {e}")
+    else:
+        print("Invalid Lambda response")
+        if status_code:
+            print(f"Status Code: {status_code}")
+    if response.get("FunctionError"):
+        print(f"{response['FunctionError']} Lambda Function Error")
+
+
+def main():
+    args = get_args()
     lambda_client = boto3.client("lambda")
-    response = invoke_lambda(lambda_client, _lambda_name(), args)
-    print(response)
-    # TODO: Handle response - details of results added to queue / error
+    response = invoke_lambda(lambda_client, lambda_name(), args)
+    handle_lambda_response(response)
 
 
 if __name__ == "__main__":
